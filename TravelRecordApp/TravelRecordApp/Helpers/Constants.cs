4555e158becaed65a28dba17bcac9433fb914cde
haitran <trannamhaibp@gmail.com>
2018-05-31 23:06:58 +0700

    implement REST foursquare services

---
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    public class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&oauth_token={2}&v={3}";
+        
+        public const string CLIENT_ID = "YAHLQZDHZIAFQOUUMPHNQXQFKZMW3ZQK33HMKQQE3SHPJUQ0";
+        
+        public const string CLIENT_SECRET = "U2K32LCMY4GT0YOEOAZSQOEERRMULOIOBQQJTJVCRFGYN34I";
+        
+        public const string OAUTH_TOKEN = "";
+    }
+}
---
+++ TravelRecordApp/TravelRecordApp/Logic/VenueLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TravelRecordApp.Helpers;
+
+namespace TravelRecordApp.Logic
+{
+    public class VenueLogic
+    {
+        public async static Task<List<Venue>> GetVenues(double latitude, double longtitude)
+        {
+            List<Venue> venues = new List<Venue>();
+
+            var url = Venue.GenerateURL(latitude, longtitude);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+            }
+            
+            return venues;
+        }
+    }
+}
